b204cff157e5d653c4ec897bc14458cbe99be76f
tracer-ics2015 <tracer@njuics.org>
2015-10-31 12:19:56 +0800

    >  compile NEMU 141220000 hyr Linux hyrathb 4.2.5-1-ARCH #1 SMP PREEMPT Tue Oct 27 08:13:28 CET 2015 x86_64 GNU/Linux  12:19:56 up  1:21,  4 users,  load average: 0.11, 0.19, 0.21 17862d41fcf7efdaa8d9b0535545608191e7a8ce

---
+++ obj/nemu/cpu/exec/data-mov/movs.d
@@ -0,0 +1,7 @@
+obj/nemu/cpu/exec/data-mov/movs.o: nemu/src/cpu/exec/data-mov/movs.c \
+ nemu/src/cpu/exec/data-mov/movs.h nemu/include/cpu/exec/helper.h \
+ nemu/include/cpu/helper.h nemu/include/nemu.h nemu/include/common.h \
+ nemu/include/debug.h nemu/include/macro.h nemu/include/memory/memory.h \
+ nemu/include/common.h nemu/include/cpu/reg.h \
+ nemu/include/cpu/decode/operand.h nemu/include/cpu/decode/decode.h \
+ nemu/include/cpu/decode/modrm.h
--- obj/nemu/cpu/exec/exec.d
+++ obj/nemu/cpu/exec/exec.d
@@ -6,27 +6,29 @@ obj/nemu/cpu/exec/exec.o: nemu/src/cpu/exec/exec.c \
  nemu/src/cpu/exec/all-instr.h nemu/src/cpu/exec/prefix/prefix.h \
  nemu/src/cpu/exec/data-mov/mov.h nemu/src/cpu/exec/data-mov/xchg.h \
  nemu/src/cpu/exec/data-mov/movsx.h nemu/src/cpu/exec/data-mov/movzx.h \
- nemu/src/cpu/exec/arith/dec.h nemu/src/cpu/exec/arith/inc.h \
- nemu/src/cpu/exec/arith/neg.h nemu/src/cpu/exec/arith/imul.h \
- nemu/src/cpu/exec/arith/mul.h nemu/src/cpu/exec/arith/idiv.h \
- nemu/src/cpu/exec/arith/div.h nemu/src/cpu/exec/arith/sub.h \
- nemu/src/cpu/exec/arith/add.h nemu/src/cpu/exec/arith/adc.h \
- nemu/src/cpu/exec/arith/sbb.h nemu/src/cpu/exec/logic/and.h \
- nemu/src/cpu/exec/logic/or.h nemu/src/cpu/exec/logic/not.h \
- nemu/src/cpu/exec/logic/xor.h nemu/src/cpu/exec/logic/sar.h \
- nemu/src/cpu/exec/logic/shl.h nemu/src/cpu/exec/logic/shr.h \
- nemu/src/cpu/exec/logic/shrd.h nemu/src/cpu/exec/logic/test.h \
- nemu/src/cpu/exec/logic/cmp.h nemu/src/cpu/exec/string/rep.h \
- nemu/src/cpu/exec/stack/push.h nemu/src/cpu/exec/stack/pop.h \
- nemu/src/cpu/exec/misc/misc.h nemu/src/cpu/exec/jmp/jmp.h \
- nemu/src/cpu/exec/jmp/je.h nemu/src/cpu/exec/jmp/jbe.h \
- nemu/src/cpu/exec/jmp/jb.h nemu/src/cpu/exec/jmp/call.h \
- nemu/src/cpu/exec/jmp/leave.h nemu/src/cpu/exec/jmp/ret.h \
- nemu/src/cpu/exec/jmp/ja.h nemu/src/cpu/exec/jmp/jae.h \
- nemu/src/cpu/exec/jmp/jcxz.h nemu/src/cpu/exec/jmp/jg.h \
- nemu/src/cpu/exec/jmp/jge.h nemu/src/cpu/exec/jmp/jl.h \
- nemu/src/cpu/exec/jmp/jle.h nemu/src/cpu/exec/jmp/jne.h \
- nemu/src/cpu/exec/jmp/jno.h nemu/src/cpu/exec/jmp/jnp.h \
- nemu/src/cpu/exec/jmp/jns.h nemu/src/cpu/exec/jmp/jo.h \
- nemu/src/cpu/exec/jmp/jp.h nemu/src/cpu/exec/jmp/js.h \
- nemu/src/cpu/exec/special/special.h
+ nemu/src/cpu/exec/data-mov/movs.h nemu/include/cpu/exec/helper.h \
+ nemu/include/cpu/decode/decode.h nemu/src/cpu/exec/data-mov/stos.h \
+ nemu/src/cpu/exec/data-mov/setcc.h nemu/src/cpu/exec/arith/dec.h \
+ nemu/src/cpu/exec/arith/inc.h nemu/src/cpu/exec/arith/neg.h \
+ nemu/src/cpu/exec/arith/imul.h nemu/src/cpu/exec/arith/mul.h \
+ nemu/src/cpu/exec/arith/idiv.h nemu/src/cpu/exec/arith/div.h \
+ nemu/src/cpu/exec/arith/sub.h nemu/src/cpu/exec/arith/add.h \
+ nemu/src/cpu/exec/arith/adc.h nemu/src/cpu/exec/arith/sbb.h \
+ nemu/src/cpu/exec/logic/and.h nemu/src/cpu/exec/logic/or.h \
+ nemu/src/cpu/exec/logic/not.h nemu/src/cpu/exec/logic/xor.h \
+ nemu/src/cpu/exec/logic/sar.h nemu/src/cpu/exec/logic/shl.h \
+ nemu/src/cpu/exec/logic/shr.h nemu/src/cpu/exec/logic/shrd.h \
+ nemu/src/cpu/exec/logic/test.h nemu/src/cpu/exec/logic/cmp.h \
+ nemu/src/cpu/exec/string/rep.h nemu/src/cpu/exec/stack/push.h \
+ nemu/src/cpu/exec/stack/pop.h nemu/src/cpu/exec/misc/misc.h \
+ nemu/src/cpu/exec/jmp/jmp.h nemu/src/cpu/exec/jmp/je.h \
+ nemu/src/cpu/exec/jmp/jbe.h nemu/src/cpu/exec/jmp/jb.h \
+ nemu/src/cpu/exec/jmp/call.h nemu/src/cpu/exec/jmp/leave.h \
+ nemu/src/cpu/exec/jmp/ret.h nemu/src/cpu/exec/jmp/ja.h \
+ nemu/src/cpu/exec/jmp/jae.h nemu/src/cpu/exec/jmp/jcxz.h \
+ nemu/src/cpu/exec/jmp/jg.h nemu/src/cpu/exec/jmp/jge.h \
+ nemu/src/cpu/exec/jmp/jl.h nemu/src/cpu/exec/jmp/jle.h \
+ nemu/src/cpu/exec/jmp/jne.h nemu/src/cpu/exec/jmp/jno.h \
+ nemu/src/cpu/exec/jmp/jnp.h nemu/src/cpu/exec/jmp/jns.h \
+ nemu/src/cpu/exec/jmp/jo.h nemu/src/cpu/exec/jmp/jp.h \
+ nemu/src/cpu/exec/jmp/js.h nemu/src/cpu/exec/special/special.h
