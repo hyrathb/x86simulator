4e2254a5a2f1dc97c1bb52d46b7b5606d2089458
hyrathb <hyrathb@gmail.com>
2015-11-02 21:13:32 +0800

    Try to use kernel

--- Makefile
+++ Makefile
@@ -9,9 +9,8 @@ CFLAGS := -MMD -Wall -Werror -c
 LIB_COMMON_DIR := lib-common
 NEWLIBC_DIR := $(LIB_COMMON_DIR)/newlib
 NEWLIBC := $(NEWLIBC_DIR)/libc.a
-#FLOAT := obj/$(LIB_COMMON_DIR)/FLOAT.a
+FLOAT := obj/$(LIB_COMMON_DIR)/FLOAT.a
 
-include config/Makefile.git
 include config/Makefile.build
 
 all: nemu
@@ -29,7 +28,6 @@ nemu: $(nemu_BIN)
 all_testcase: $(testcase_BIN)
 kernel: $(kernel_BIN)
 game: $(game_BIN)
-
 
 ##### rules for cleaning the project #####
 
@@ -52,8 +50,8 @@ clean: clean-cpp
 
 ##### some convinient rules #####
 
-USERPROG := obj/testcase/movsx
-ENTRY := $(USERPROG)
+USERPROG := obj/testcase/mov-c
+ENTRY := $(kernel_BIN)
 
 entry: $(ENTRY)
 	objcopy -S -O binary $(ENTRY) entry
--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,24 +31,20 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x7f454c46;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+        ph = (void *) ((char *)buf + elf->e_phoff);
+        int i;
+	for(i=0; i< elf->e_phnum; ++i) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-
-			/* TODO: read the content of the segment from the ELF file 
-			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
-			 */
-			 
-			 
-			/* TODO: zero the memory region 
-			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
+                    char *mem = (char *)mm_malloc(ph->p_vaddr, ph->p_memsz);
+                    ramdisk_read((uint8_t *)mem, ph->p_offset, ph->p_filesz);
+                    memset(mem+ph->p_filesz, 0, ph->p_memsz-ph->p_filesz);
 
 
 #ifdef IA32_PAGE
